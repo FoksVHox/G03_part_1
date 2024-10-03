0ba48a7f654c0a97aaa292711ced239c101e8a28
mikkel <s244732@dtu.dk>
2024-10-03 12:04:21 +0200

    I have made a class called raffle where it rolls the dice twice and adds the points together.

--- src/Raffle.java
+++ src/Raffle.java
@@ -5,10 +5,22 @@ import java.util.ArrayList;
 public class Raffle {
     private ArrayList<Die> dice;
 
-    private void shuffle(){
-        throw new UnsupportedOperationException("not implemented yet");
+    public Raffle() {
+        for (int i=0;i<2;i++){
+            dice.add(new Die());
+        }
+
+
     }
     public int[] Raffle(){
-        throw new UnsupportedOperationException("not implemented yet");
+        int [] points = new int[2];
+        int counter = 0;
+        for (Die indiviualDie : dice){
+            points[counter] = indiviualDie.Roll();
+            counter++;
+
+        }
+        return points;
     }
+
 }
