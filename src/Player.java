3455d475126ec887a0bd6eb75d392d31b764afac
hvimsk <Mathiasmagnussen@hotmail.dk>
2024-10-03 11:21:37 +0200

    extended player class

--- src/Player.java
+++ src/Player.java
@@ -6,6 +6,12 @@ public class Player {
     public int GetScore(){
         throw new UnsupportedOperationException("not implemented yet");
     }
+    public int GetPlayerPosition(){
+        throw new UnsupportedOperationException("not implemented yet");
+    }
+    public void SetPlayerPosition(int position){
+        this.playerPosition = position;
+    }
     public void SetScore(int score){
         throw new UnsupportedOperationException("not implemented yet");
     }
