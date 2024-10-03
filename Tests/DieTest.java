ee4fcf0fd957bdd5553fa818470ddd841ecc752f
hvimsk <Mathiasmagnussen@hotmail.dk>
2024-10-03 09:39:12 +0200

    Added DieTest to Code. should in theory be complete

--- Tests/DieTest.java
+++ Tests/DieTest.java
@@ -1,13 +1,47 @@
 package Tests;
 
+import Tests.Utility.DistributionOfRolls;
 import org.junit.jupiter.api.Test;
+import src.Die;
 
 import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 class DieTest {
 
     @Test
     void roll() {
-        throw new UnsupportedOperationException("not implemented yet");
+        Die individiulDie = new Die();
+        int faceValue = individiulDie.Roll();
+
+        int lowerBound = 1;
+        int upperBound = 6;
+
+
+        assertTrue(faceValue >= lowerBound && faceValue <= upperBound, "Value is not in range!");
     }
+
+    @Test
+    void Roll1000Times(){
+        Die individiulDie = new Die();
+        DistributionOfRolls utilityClass = new DistributionOfRolls();
+
+
+
+        for (int i=0;i<1000;i++ ){
+            utilityClass.addValue(individiulDie.Roll());
+        }
+
+
+        for (double PercentageDistribution : utilityClass.calculateDistribution()){
+            assertTrue(20<PercentageDistribution&& PercentageDistribution<30);
+
+        }
+
+    }
+
+
+
+
+
 }
