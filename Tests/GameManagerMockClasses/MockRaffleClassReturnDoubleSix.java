d953c55b3dafaa2bb57c3289095c906b688b0d30
hvimsk <Mathiasmagnussen@hotmail.dk>
2024-10-03 10:45:51 +0200

    MockRaffleClassReturnDoubleSix

---
+++ Tests/GameManagerMockClasses/MockRaffleClassReturnDoubleSix.java
@@ -0,0 +1,10 @@
+package Tests.GameManagerMockClasses;
+
+import src.Raffle;
+
+public class MockRaffleClassReturnDoubleSix extends Raffle {
+
+    public int[] Raffle (){
+        return new int[]{6,6};
+    }
+}
