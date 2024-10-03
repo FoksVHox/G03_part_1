d1985fb086e7c052385e1e717e2dbe5fc3dc3fc4
hvimsk <Mathiasmagnussen@hotmail.dk>
2024-10-03 11:11:06 +0200

    changed Output of StartGame to String. the string is the player who won

--- src/GameManager.java
+++ src/GameManager.java
@@ -6,14 +6,16 @@ public class GameManager {
     private Raffle raffle;
     private ArrayList<Player> players;
 
-    public GameManager(int amountOfPlayers) {
+    public GameManager(int amountOfPlayers, Raffle Inputraffle) {
+        raffle = Inputraffle;
         throw new UnsupportedOperationException("not implemented yet");
+
     }
 
     private void setPlayerScore(){
         throw new UnsupportedOperationException("not implemented yet");
     }
-    public void StartGame(){
+    public String StartGame(){
         throw new UnsupportedOperationException("not implemented yet");
     }
 }
