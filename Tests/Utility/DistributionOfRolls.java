478c802e50ec5a1bd27922fa0683acb0e50592b5
hvimsk <Mathiasmagnussen@hotmail.dk>
2024-10-03 09:17:17 +0200

    DistributionOfRolls class for nicklas

---
+++ Tests/Utility/DistributionOfRolls.java
@@ -0,0 +1,27 @@
+package Tests.Utility;
+
+public class DistributionOfRolls {
+    int[] AmountFaceValue = {1,2,3,4,5,6};
+
+
+
+
+    public void addValue(int FaceValue){
+        AmountFaceValue[FaceValue-1]++;
+    }
+    public int[] calculateDistribution(){
+        int[] distribution = new int[AmountFaceValue.length];
+
+
+         
+
+        return distribution;
+    }
+
+    private int calculateSumOfAmountFaceValue(){
+
+        int sum = 0;
+        
+
+    }
+}
