da1516e731b6d66cb800113113b0f5cc7c02b8da
hvimsk <Mathiasmagnussen@hotmail.dk>
2024-10-03 17:34:14 +0200

    Last tests added plus Mocking implementation of Raffles

---
+++ Tests/GameManagerMockClasses/MockRaffleDoubleOnes.java
@@ -0,0 +1,34 @@
+package Tests.GameManagerMockClasses;
+
+import src.Raffle;
+
+import java.util.ArrayList;
+
+public class MockRaffleDoubleOnes extends Raffle {
+
+    int currentIndOfListArray = 0;
+    ArrayList<int[]> ListOfInputs;
+
+    public MockRaffleDoubleOnes() {
+        ListOfInputs = new ArrayList<>();
+
+        ListOfInputs.add(new int[]{6,5}); //player 1, 11 points
+        ListOfInputs.add(new int[]{6,5});//player 2, 11 points
+        ListOfInputs.add(new int[]{6,5});//player 1, 22 points
+        ListOfInputs.add(new int[]{6,5});//player 2, 22 points
+        ListOfInputs.add(new int[]{6,5});//player 1, 33 points
+        ListOfInputs.add(new int[]{6,5});//player 2, 33 points
+        ListOfInputs.add(new int[]{1,1});//player 1, 0 points
+        ListOfInputs.add(new int[]{6,6}); // player 1 should not win
+        ListOfInputs.add(new int[]{5,4});//player 2, 42 points
+        ListOfInputs.add(new int[]{5,3});
+        ListOfInputs.add(new int[]{6,6});// player 2 should win
+
+
+
+    }
+    public int[] Raffle (){
+        currentIndOfListArray++;
+        return ListOfInputs.get(currentIndOfListArray);
+    }
+}
--- Tests/GameManagerTest.java
+++ Tests/GameManagerTest.java
@@ -1,26 +1,15 @@
 package Tests;
 
-import Tests.GameManagerMockClasses.MockRaffleClassReturnDoubleSix;
-import org.junit.jupiter.api.Nested;
+import Tests.GameManagerMockClasses.*;
 import org.junit.jupiter.api.Test;
 import src.GameManager;
 import src.Player;
 import src.Raffle;
-
-import java.io.ByteArrayInputStream;
-
-import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 class GameManagerTest {
 
-    @Test
-    void StartGame() {
 
-
-        String simulatedInput = "\n";
-        ByteArrayInputStream inputStream = new ByteArrayInputStream(simulatedInput.getBytes());
-
-    }
 
     @Test
     void Player1Wins(){
@@ -29,7 +18,7 @@ class GameManagerTest {
 
         Player playerWhoWon = sut.StartGame();
 
-        assertTrue(playerWhoWon.playerPosition == 1);
+        assertEquals(1, playerWhoWon.playerPosition);
 
 
 
@@ -37,26 +26,51 @@ class GameManagerTest {
 
     @Test
     void gameEndIfDouble6TwoTimesInARow(){
+        Raffle mockRaffle = new MockRafflePlayer2Wins();
+        GameManager sut = new GameManager(2,mockRaffle);
 
+        Player playerWhoWon = sut.StartGame();
 
+        assertEquals(2, playerWhoWon.playerPosition);
     }
 
 
     @Test
     void playerResetPointsIfDouble1(){
 
+        //This mocks allocates 33 points to player 1 then double 1 and SUT should
+        // handle not awarding victory afterwards
+        Raffle mockRaffle = new MockRaffleDoubleOnes();
+        GameManager sut = new GameManager(2,mockRaffle);
+
+        Player playerWhoWon = sut.StartGame();
+
+        assertEquals(2, playerWhoWon.playerPosition);
+
     }
 
 
     @Test
     void playerRollGetOneMoreRollIfDoubleRoll(){
+        Raffle mockRaffle = new MockRaffleDouble_extraTurn();
+        GameManager sut = new GameManager(2,mockRaffle);
 
+        Player playerWhoWon = sut.StartGame();
+
+        assertEquals(1, playerWhoWon.playerPosition);
     }
 
 
     @Test
     void toWinPlayerHaveTooRollDoubleAfter40PointsToWin(){
-        //
+
+        Raffle mockRaffle = new MockRaffleDouble_extraTurn();
+        GameManager sut = new GameManager(2,mockRaffle);
+
+        Player playerWhoWon = sut.StartGame();
+
+        assertEquals(1, playerWhoWon.playerPosition);
+
     }
 
 
