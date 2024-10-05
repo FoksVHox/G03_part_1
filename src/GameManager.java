b8da9893d3cdb0741fae0e4acd0833311bc15882
Stardust_Blooms <signenielsen8888@outlook.com>
2024-10-05 21:18:04 +0200

    Base for GameManager

--- src/GameManager.java
+++ src/GameManager.java
@@ -6,14 +6,35 @@ public class GameManager {
     private Raffle raffle;
     private ArrayList<Player> players;
 
-    public GameManager(int amountOfPlayers) {
+    public GameManager(int amountOfPlayers, Raffle Inputraffle) {
+
+        var scanner = new java.util.Scanner(System.in);
+        scanner.useLocale(java.util.Locale.ENGLISH);
+        int amountOfPlayers = scanner.nextInt();
+
+        raffle = Inputraffle;
         throw new UnsupportedOperationException("not implemented yet");
+
     }
 
     private void setPlayerScore(){
         throw new UnsupportedOperationException("not implemented yet");
+        int currentScore = GetScore();
+
+
     }
-    public void StartGame(){
+    public Player StartGame(){
         throw new UnsupportedOperationException("not implemented yet");
+        //While loop to keep the game running
+        while () {
+            // Win condition 1 player
+            if (score == 40) {
+                System.out.println(player + "wins");
+            // Win condition 2
+            } else if () {
+
+            }
+
+        }
     }
 }
